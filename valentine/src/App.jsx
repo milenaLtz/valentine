df7fd05cba8d0e5cbc8afa3bd5b2275e320a2bd7
milenaLtz <milena.letz@gmail.com>
2026-02-14 01:13:13 +0100

    feat: App changed

--- valentine/src/App.jsx
+++ valentine/src/App.jsx
@@ -9,14 +9,12 @@ import MemorySlider from './widgets/MemorieSlider';
 
 
 function App() {
-
   const [step, setStep] = useState(0);
 
   if (step === 0) return <div className='content-block'><Intro next={() => setStep(1)} /></div>;
   if (step === 1) return <div className='content-block'><Quiz next={() => setStep(2)} /></div>;
   if (step === 2) return <div className='content-block'><MemorySlider next={() => setStep(3)} /></div>;
   if (step === 3) return <div className='content-block'><ValentineDate /></div>;
-
 }
 
 export default App
